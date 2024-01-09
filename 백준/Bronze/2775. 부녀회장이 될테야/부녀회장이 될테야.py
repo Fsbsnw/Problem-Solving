19d093b0b5b70f1c92671897f96845f5043f9e86
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2024-01-10 01:21:12 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -5,16 +5,9 @@ t = int(input())
 for i in range(t):
     k = int(input())
     n = int(input())
-    mmax = max(n, k)
-    arr = [[0] * (mmax + 1) for i in range(mmax + 1)]
-    
-    for i in range(len(arr[0])):
-        arr[0][i] = i
-    
-    for i, val1 in enumerate(arr):
-        if i == 0:
-            continue
-        for j, val2 in enumerate(val1):
-            arr[i][j] = sum(arr[i - 1][:j + 1])
+    arr = [i for i in range(1, n + 1)]
 
-    print(arr[k][n])
+    for i in range(k):
+        for j in range(1, n):
+            arr[j] += arr[j - 1]
+    print(arr[n - 1])
