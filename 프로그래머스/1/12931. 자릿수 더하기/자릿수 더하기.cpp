de04c93fa5fd40096003a3351ec47add8c169e9c
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2024-01-22 23:32:58 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    string s = to_string(n);
+    for (char c : s)
+    {
+        answer += int(c - '0');
+    }
+    return answer;
+}
