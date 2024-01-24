2abfea04a46b2bee7f1eb065216617249f6fc6a5
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2024-01-24 15:57:43 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -5,16 +5,11 @@ using namespace std;
 
 bool solution(int x) {
     int temp = x, result = 0;
-    bool answer = true;
     
     while (temp > 0)
     {
         result += temp % 10;
         temp /= 10;
     }
-    if (x % result != 0)
-    {
-        answer = false;
-    }    
-    return answer;
+    return x % result == 0;
 }
