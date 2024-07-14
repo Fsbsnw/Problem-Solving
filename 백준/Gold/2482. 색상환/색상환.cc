9fe82a47bb5a39c823f628731b74692cf775d392
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2024-07-14 16:45:54 +0900

    [Gold III] Title: 색상환, Time: 4 ms, Memory: 5936 KB -BaekjoonHub

---
+++ 백준/Gold/2482. 색상환/색상환.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int n, k;
+const int mod = 1000000003;
+int dp[1001][1001];
+
+int main()
+{
+    cin >> n >> k;
+    
+    for (int i = 0; i <= n; i++) 
+    {
+        dp[i][0] = 1;
+        dp[i][1] = i;
+    }
+    
+    for (int i = 2; i < n; i++)
+    {
+        for (int j = 2; j <= k; j++)
+        {
+            dp[i][j] = (dp[i - 2][j - 1] + dp[i - 1][j]) % mod;
+        }
+    }
+    
+    cout << (dp[n - 1][k] + dp[n - 3][k - 1]) % mod << '\n';
+
+    return 0;
+}
