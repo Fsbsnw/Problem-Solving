a70e6efc8f0ab60b6d1573bf12e5a4daab04245c
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2025-07-19 19:12:14 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 52 ms, Memory: 22336 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int PrefixSum[26][200001];
+
+int main()
+{
+    string Text;
+    cin >> Text;
+    
+    for (int Alpha = 0; Alpha < 26; Alpha++)
+    {
+        int* AlphaSum = PrefixSum[Alpha];
+        
+        for (int Index = 0; Index < Text.size(); Index++)
+        {
+            if (Index == 0)
+            {
+                AlphaSum[Index] = (Text[Index] - 'a' == Alpha) ? 1 : 0;
+            }
+            else
+            {
+                int Alphabet = Text[Index] - 'a';
+                
+                AlphaSum[Index] = (Alpha == Alphabet) ? AlphaSum[Index - 1] + 1 : AlphaSum[Index - 1];
+            }
+        }
+    }
+    
+    int T;
+    cin >> T;
+    
+    for (int Case = 0; Case < T; Case++)
+    {
+        char Character;
+        cin >> Character;
+        
+        int Left, Right;
+        cin >> Left >> Right;
+        
+        int CharIndex = Character - 'a';
+        if (Left == 0)
+        {
+            cout << PrefixSum[CharIndex][Right] << '\n';
+        }
+        else
+        {
+            cout << PrefixSum[CharIndex][Right] - PrefixSum[CharIndex][Left - 1] << '\n';
+        }
+    }
+
+    return 0;
+}
