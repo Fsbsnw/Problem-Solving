b290bea90ac688f05d52ab6487755e6ab5c5e3e1
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2025-07-31 23:54:24 +0900

    [Silver V] Title: 타일 장식물, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13301. 타일 장식물/타일 장식물.cc
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.cc
@@ -1,11 +1,13 @@
 #include<iostream>
 
+using namespace std;
+
 int main()
 {
     int N;
-    scanf("%d", &N);
+    cin >> N;
     
-    long long DP[41];
+    long long DP[81];
     
     DP[1] = 1;
     DP[2] = 1;
@@ -15,9 +17,8 @@ int main()
         DP[i] = DP[i - 2] + DP[i - 1];
     }
     
-    long long Result = DP[N] * 2 + (DP[N - 1] + DP[N]) * 2;
-    
-    printf("%d", Result);
+    long long Result = (N == 1) ? 4 : 2 * (DP[N] + DP[N] + DP[N - 1]);
+    cout << Result;
 
     return 0;
 }
