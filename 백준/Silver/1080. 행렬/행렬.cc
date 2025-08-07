7cf4c13affba92ee69998cc9804015f1f922b21d
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2025-08-07 21:49:53 +0900

    [Silver I] Title: 행렬, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1080. 행렬/행렬.cc
+++ 백준/Silver/1080. 행렬/행렬.cc
@@ -1,74 +1,64 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-void Reverse(int& Value)
+void InputMatrix(vector<vector<int>>& Matrix, int N, int M) 
 {
-    Value = (Value == 0) ? 1 : 0;
+    string Line;
+    for (int i = 0; i < N; i++) 
+    {
+        cin >> Line;
+        for (int j = 0; j < M; j++) 
+        {
+            Matrix[i][j] = Line[j] - '0';
+        }
+    }
 }
 
-int main()
+bool IsSame(const vector<vector<int>>& A, const vector<vector<int>>& B, int N, int M) 
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            if (A[i][j] != B[i][j]) return false;
+        }
+    }
+    return true;
+}
+
+int main() 
 {
     int N, M;
     cin >> N >> M;
-    
+
     vector<vector<int>> InitialMatrix(N, vector<int>(M));
     vector<vector<int>> TargetMatrix(N, vector<int>(M));
-    string line;
 
-    for (int i = 0; i < N; i++)
-    {
-        cin >> line;
-        for (int j = 0; j < M; j++)
-        {
-            InitialMatrix[i][j] = line[j] - '0';
-        }
-    }
-    
-    for (int i = 0; i < N; i++)
-    {
-        cin >> line;
-        for (int j = 0; j < M; j++)
-        {
-            TargetMatrix[i][j] = line[j] - '0';
-        }
-    }
-    
+    InputMatrix(InitialMatrix, N, M);
+    InputMatrix(TargetMatrix, N, M);
+
     int CalcCount = 0;
-    for (int i = 0; i < N - 2; i++)
+    for (int i = 0; i <= N - 3; i++) 
     {
-        for (int j = 0; j < M - 2; j++)
+        for (int j = 0; j <= M - 3; j++) 
         {
-            if (InitialMatrix[i][j] == TargetMatrix[i][j]) continue;
-            
-            for (int x = i; x < i + 3; x++)
+            if (InitialMatrix[i][j] != TargetMatrix[i][j]) 
             {
-                for (int y = j; y < j + 3; y++)
+                for (int x = i; x < i + 3; x++)
                 {
-                    Reverse(InitialMatrix[x][y]);
+                    for (int y = j; y < j + 3; y++)
+                    {
+                        InitialMatrix[x][y] ^= 1;
+                    }
                 }
-            }
-            CalcCount++;
-        }
-    }
-    
-    bool bSame = true;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            if (InitialMatrix[i][j] != TargetMatrix[i][j])
-            {
-                bSame = false;
-                break;
+                CalcCount++;
             }
         }
-        if (!bSame) break;
     }
-    
-    int Result = (bSame) ? CalcCount : -1;
-    cout << Result;
 
+    cout << (IsSame(InitialMatrix, TargetMatrix, N, M) ? CalcCount : -1);
     return 0;
 }
