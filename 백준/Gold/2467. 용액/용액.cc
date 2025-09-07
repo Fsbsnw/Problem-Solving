b9637e6ca827c7224171772cb7e3b4992fde7a13
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2025-09-07 22:11:57 +0900

    [Gold V] Title: 용액, Time: 56 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.cc
+++ 백준/Gold/2467. 용액/용액.cc
@@ -10,44 +10,41 @@ int main()
     
     vector<int> Liquids(N);
     
-    int Num1, Num2;
-    
     for (int i = 0; i < N; i++)
     {
         cin >> Liquids[i];
     }
     
     int MIN = 2000000000;
-    
-    for (int i = 0; i < N; i++)
-    {
-        int Left = i + 1;
-        int Right = N - 1;
+    int Num1, Num2;
         
-        while (Left <= Right)
+    int Left = 0;
+    int Right = N - 1;
+    
+    while (Left < Right)
+    {
+        int Sum = Liquids[Left] + Liquids[Right];
+        if (abs(Sum) < MIN)
         {
-            int Mid = (Left + Right) / 2;
-            int Sum = Liquids[i] + Liquids[Mid];
-            int Target = abs(Sum);
-            
-            if (Target < MIN)
-            {
-                MIN = Target;
-                Num1 = Liquids[i];
-                Num2 = Liquids[Mid];
-            }
-            
-            if (Sum > 0)
-            {
-                Right = Mid - 1;
-            }
-            else
-            {
-                Left = Mid + 1;
-            }
+            MIN = abs(Sum);
+            Num1 = Liquids[Left];
+            Num2 = Liquids[Right];
+        }
+        
+        if (Sum > 0)
+        {
+            Right--;
+        }
+        else if (Sum < 0)
+        {
+            Left++;
+        }
+        else
+        {
+            break;
         }
     }
-    
+        
     cout << Num1 << ' ' << Num2;
 
     return 0;
