003b51b766e71dc3e0da81301cfdd371513309b8
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2025-09-10 14:56:32 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -2,32 +2,36 @@
 #include <vector>
 using namespace std;
 
+int N, S;
+int Count = 0;
+vector<int> Arr;
+
+void DFS(int Index, int Sum)
+{
+    if (Index == N)
+    {
+        if (Sum == S) Count++;
+        return;
+    }
+    
+    DFS(Index + 1, Sum + Arr[Index]);
+    DFS(Index + 1, Sum);
+}
+
 int main()
 {
-    int N, S;
     cin >> N >> S;
     
-    vector<int> Arr(N);
+    Arr.resize(N);
     
     for (int i = 0; i < N; i++)
     {
         cin >> Arr[i];
     }
     
-    int Count = 0;
+    DFS(0, 0);
     
-    for (int i = 1; i < (1 << N); i++)
-    {
-        int Sum = 0;
-        for (int j = 0; j < N; j++)
-        {
-            if (i & (1 << j))
-            {
-                Sum += Arr[j];
-            }
-        }
-        if (Sum == S) Count++;
-    }
+    if (S == 0) Count--;
     
     cout << Count;
 
