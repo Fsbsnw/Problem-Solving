c430689636a49395c3ad852e65499caa74aaff1c
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2025-10-25 11:51:55 +0900

    [level 2] Title: 소수 찾기, Time: 0.84 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+bool IsPrime(int n)
+{
+    if (n < 2) return false;
+    for (int i = 2; i * i <= n; ++i)
+    {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+set<int> SetNum;
+vector<bool> Used;
+
+void FindPrime(string s, string numbers)
+{
+    if (!s.empty())
+    {
+        int n = stoi(s);
+        if (IsPrime(n)) SetNum.insert(n);
+    }
+    
+    for (int i = 0; i < numbers.size(); ++i)
+    {
+        if (Used[i]) continue;
+        
+        Used[i] = true;
+        s.push_back(numbers[i]);
+        FindPrime(s, numbers);
+        s.pop_back();
+        Used[i] = false;
+    }
+}
+
+int solution(string numbers) {
+    Used.resize(numbers.size());
+    
+    FindPrime("", numbers);
+    
+    int answer = SetNum.size();
+    return answer;
+}
