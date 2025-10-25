a450357c87c02b29c83280a19d8c0c94d292e671
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2025-10-25 11:08:59 +0900

    [level 1] Title: 모의고사, Time: 0.08 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.05 ms
+메모리: 4.15 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 19:19:28
+2025년 10월 25일 11:08:51
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -5,31 +5,26 @@
 using namespace std;
 
 vector<int> solution(vector<int> answers) {
+    vector<int> Base1 = {1, 2, 3, 4, 5};
+    vector<int> Base2 = {2, 1, 2, 3, 2, 4, 2, 5};
+    vector<int> Base3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    
+    int Count1 = 0, Count2 = 0, Count3 = 0;
+    
+    for (int i = 0; i < answers.size(); ++i)
+    {
+        if (answers[i] == Base1[i % 5]) ++Count1;
+        if (answers[i] == Base2[i % 8]) ++Count2;
+        if (answers[i] == Base3[i % 10]) ++Count3;
+    }
+    
     vector<int> answer;
-    int arr1[5] = {1, 2, 3, 4, 5};
-    int arr2[8] = {2, 1, 2, 3, 2, 4, 2, 5};
-    int arr3[10] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-    int idx = 0, cnt1 = 0, cnt2 = 0, cnt3 = 0;
     
-    for (int i = 0; i < answers.size(); i++) {
-        if (arr1[i % 5] == answers[i]) {
-            cnt1++;
-        }
-        if (arr2[i % 8] == answers[i]) {
-            cnt2++;
-        }
-        if (arr3[i % 10] == answers[i]) {
-            cnt3++;
-        }
-    }
-    int arr[3] = {cnt1, cnt2, cnt3};
-    int* maxV = max_element(arr, end(arr));
+    int MaxCount = max({Count1, Count2, Count3});
     
-    for (int i = 0; i < 3; i++) {
-        if (*maxV == arr[i]) {
-            answer.push_back(i + 1);
-        }
-    }
+    if (Count1 == MaxCount) answer.push_back(1);
+    if (Count2 == MaxCount) answer.push_back(2);
+    if (Count3 == MaxCount) answer.push_back(3);    
     
     return answer;
 }
