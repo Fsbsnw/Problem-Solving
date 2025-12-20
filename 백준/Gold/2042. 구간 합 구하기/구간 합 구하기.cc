de8d5ed77d4385aada17bbbd94b4490b06aa4ebb
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2025-12-20 16:51:28 +0900

    [Gold I] Title: 구간 합 구하기, Time: 280 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -5,44 +5,44 @@ using namespace std;
 typedef long long ll;
 
 int N, M, K;
-ll Tree[1000000 * 4];
 ll Arr[1000001];
+ll Tree[1000000 * 4];
 
-void InitTree(int Node, int Left, int Right, int Index)
+void TreeInsert(int Node, int Left, int Right, int Index)
 {
-    if (Index < Left || Index > Right) return;
+    if (Index < Left || Right < Index) return;
     
     Tree[Node] += Arr[Index];
     
     if (Left != Right)
     {
         int Mid = (Left + Right) / 2;
-        InitTree(Node * 2, Left, Mid, Index);
-        InitTree(Node * 2 + 1, Mid + 1, Right, Index);
+        TreeInsert(Node * 2, Left, Mid, Index);
+        TreeInsert(Node * 2 + 1, Mid + 1, Right, Index);
     }
 }
 
-void UpdateTree(int Node, int Left, int Right, int Index, ll Diff)
+void TreeUpdate(int Node, int Left, int Right, int Index, ll Diff)
 {
-    if (Index < Left || Index > Right) return;
+    if (Index < Left || Right < Index) return;
     
     Tree[Node] += Diff;
     
-    if (Left != Right)
-    {
+   if (Left != Right)
+   {
         int Mid = (Left + Right) / 2;
-        UpdateTree(Node * 2, Left, Mid, Index, Diff);
-        UpdateTree(Node * 2 + 1, Mid + 1, Right, Index, Diff);
-    }
+        TreeUpdate(Node * 2, Left, Mid, Index, Diff);
+        TreeUpdate(Node * 2 + 1, Mid + 1, Right, Index, Diff);
+   }
 }
 
-ll GetSum(int Node, int Left, int Right, int Start, int End)
+ll TreeSum(int Node, int Left, int Right, int Start, int End)
 {
-    if (End < Left || Right < Start) return 0;
+    if (Right < Start || End < Left) return 0;
     if (Start <= Left && Right <= End) return Tree[Node];
     
     int Mid = (Left + Right) / 2;
-    return GetSum(Node * 2, Left, Mid, Start, End) + GetSum(Node * 2 + 1, Mid + 1, Right, Start, End);
+    return TreeSum(Node * 2, Left, Mid, Start, End) + TreeSum(Node * 2 + 1, Mid + 1, Right, Start, End);
 }
 
 int main()
@@ -57,25 +57,27 @@ int main()
     {
         cin >> Arr[i];
         
-        InitTree(1, 0, N - 1, i);
+        TreeInsert(1, 0, N - 1, i);
     }
     
     for (int i = 0; i < M + K; ++i)
     {
         int a, b;
         ll c;
+        
         cin >> a >> b >> c;
         
         if (a == 1)
         {
             ll Diff = c - Arr[b - 1];
             Arr[b - 1] = c;
-            UpdateTree(1, 0, N - 1, b - 1, Diff);
+            TreeUpdate(1, 0, N - 1, b - 1, Diff);
         }
         else
         {
-            cout << GetSum(1, 0, N - 1, b - 1, c - 1) << '\n';
+            cout << TreeSum(1, 0, N - 1, b - 1, c - 1) << '\n';
         }
     }
+
     return 0;
 }
