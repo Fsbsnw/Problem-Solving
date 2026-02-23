f25d444ae4513472be22420a448cde7a46e87f2a
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2026-02-23 23:12:22 +0900

    [level 2] Title: 구명보트, Time: 1.96 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -5,30 +5,20 @@
 using namespace std;
 
 int solution(vector<int> people, int limit) 
-{
-    vector<bool> rescued(people.size());
-    
+{    
     sort(people.begin(), people.end());
     
     int answer = 0;
-    
     int li = 0, ri = people.size() - 1;
     
     while (li <= ri)
     {
-        if (li == ri)
-        {
-            ++answer;
-            break;
-        }
         if (people[li] + people[ri] <= limit)
         {
             ++li;
         }
         --ri;
         ++answer;
-    }
-    
-    
+    }    
     return answer;
 }
