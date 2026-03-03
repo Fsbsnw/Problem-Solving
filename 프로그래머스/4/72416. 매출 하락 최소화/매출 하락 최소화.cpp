6f7a66e77229ef6099e1f0c1cfe6a58939501753
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2026-03-03 17:24:33 +0900

    [level 4] Title: 매출 하락 최소화, Time: 231.86 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/4/72416. 매출 하락 최소화/매출 하락 최소화.cpp
+++ 프로그래머스/4/72416. 매출 하락 최소화/매출 하락 최소화.cpp
@@ -12,8 +12,8 @@ struct FNode
 {
     FNode(int InID, int InSale) : ID(InID), Sale(InSale) {};
     
-    int ID = 0;
-    int Sale = 0;
+    int ID;
+    int Sale;
     vector<FNode*> Children;
 };
 
@@ -26,7 +26,7 @@ public:
         Index[RootID] = Root;
     }
     
-    void Insert(int ParentID, int ParentSale, int ChildID, int ChildSale)
+    void InsertRelation(int ParentID, int ParentSale, int ChildID, int ChildSale)
     {
         FNode* PNode = GetOrCreate(ParentID, ParentSale);
         FNode* CNode = GetOrCreate(ChildID, ChildSale);
@@ -34,14 +34,10 @@ public:
         PNode->Children.push_back(CNode);
     }
     
-    int CalcDP()
+    int GetMinSalesDecline()
     {
-        return DFS(Root);
-    }
-    
-    void Print()
-    {
-        DFSPrint(Root, 0);
+        CalcMinSales(Root);
+        return min(DP[Root->ID][0], DP[Root->ID][1]);
     }
     
 private:
@@ -56,46 +52,29 @@ private:
         return Index[ID];
     }
     
-    int DFS(FNode* Node)
-    {        
+    void CalcMinSales(FNode* Node)
+    {
+        DP[Node->ID][1] = Node->Sale;
+        
         int Sum_Sales = 0;
-        int Min_Child = INT_MAX;
+        int Min_SalesDecline = INT_MAX;
         bool bAtLeastOne = false;
         
         for (FNode* Child : Node->Children)
         {
-            DFS(Child);
+            CalcMinSales(Child);
             int Child0 = DP[Child->ID][0];
             int Child1 = DP[Child->ID][1];
             
             if (Child1 <= Child0) bAtLeastOne = true;
             Sum_Sales += min(Child0, Child1);
-            Min_Child = min(Min_Child, Child1 - Child0);
+            Min_SalesDecline = min(Min_SalesDecline, Child1 - Child0);
         }
         
-        DP[Node->ID][1] = Sum_Sales + Node->Sale;
-        if (Node->Children.empty()) 
-        {
-            DP[Node->ID][0] = 0;
-            return 0;
-        }
-        
+        DP[Node->ID][1] += Sum_Sales;
+        if (Node->Children.empty()) return;
         if (bAtLeastOne) DP[Node->ID][0] = Sum_Sales;
-        else DP[Node->ID][0] = Sum_Sales + Min_Child;
-        
-        return min(DP[Node->ID][1], DP[Node->ID][0]);
-    }
-    
-    void DFSPrint(FNode* Node, int Depth)
-    {
-        cout << Node->ID << " (Sale : " << Node->Sale << ")\n";
-        
-        for (FNode* Child : Node->Children)
-        {
-            for (int i = 0; i <= Depth; ++i) 
-                cout << "   ";
-            DFSPrint(Child, Depth + 1);
-        }
+        else DP[Node->ID][0] = Sum_Sales + Min_SalesDecline;
     }
     
     unordered_map<int, FNode*> Index;
@@ -107,12 +86,7 @@ int solution(vector<int> sales, vector<vector<int>> links) {
     
     for (vector<int> Pair : links)
     {
-        Tree->Insert(Pair[0], sales[Pair[0] - 1], Pair[1], sales[Pair[1] - 1]);
+        Tree->InsertRelation(Pair[0], sales[Pair[0] - 1], Pair[1], sales[Pair[1] - 1]);
     }    
-    
-    int answer = 0;
-    
-    answer = Tree->CalcDP();
-    
-    return answer;
+    return Tree->GetMinSalesDecline();
 }
