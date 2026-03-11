c4aa87565c0346980c90379e600031eae3f59b58
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2026-03-11 21:26:40 +0900

    [Gold I] Title: 구간 합 구하기, Time: 1032 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -5,55 +5,46 @@ using namespace std;
 typedef long long ll;
 
 ll Arr[1000001];
-ll Tree[4000001];
+ll Seg[4000001];
 
-ll Init(int Left, int Right, int Node)
+ll Init(int Node, int Left, int Right)
 {
-    if (Left == Right) return Tree[Node] = Arr[Left];
+    if (Left == Right) return Seg[Node] = Arr[Left];
     
     int Mid = (Left + Right) / 2;
-    
-    return Tree[Node] = Init(Left, Mid, Node * 2) + Init(Mid + 1, Right, Node * 2 + 1);
+    return Seg[Node] = Init(Node * 2, Left, Mid) + Init(Node * 2 + 1, Mid + 1, Right);
 }
 
-void Update(int Left, int Right, int Node, int Index, ll Diff)
+ll Sum(int Node, int Left, int Right, int Start, int End)
+{
+    if (End < Left || Right < Start) return 0;
+    if (Start <= Left && Right <= End) return Seg[Node];
+    
+    int Mid = (Left + Right) / 2;
+    return Sum(Node * 2, Left, Mid, Start, End) + Sum(Node * 2 + 1, Mid + 1, Right, Start, End);
+}
+
+void Update(int Node, int Left, int Right, int Index, ll Diff)
 {
     if (Index < Left || Right < Index) return;
     
-    Tree[Node] += Diff;
+    Seg[Node] += Diff;
     
     if (Left == Right) return;
     
     int Mid = (Left + Right) / 2;
-    
-    Update(Left, Mid, Node * 2, Index, Diff);
-    Update(Mid + 1, Right, Node * 2 + 1, Index, Diff);
-}
-
-ll Sum(int Left, int Right, int Start, int End, int Node)
-{
-    if (End < Left || Right < Start) return 0;
-    if (Start <= Left && Right <= End) return Tree[Node];
-    
-    int Mid = (Left + Right) / 2;
-    return Sum(Left, Mid, Start, End, Node * 2) + Sum(Mid + 1, Right, Start, End, Node * 2 + 1);
+    Update(Node * 2, Left, Mid, Index, Diff);
+    Update(Node * 2 + 1, Mid + 1, Right, Index, Diff);
 }
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    
-    ll N, M, K;
+    int N, M, K;
     cin >> N >> M >> K;
     
-    for (int i = 1; i <= N; ++i) 
-    {
-        cin >> Arr[i];
-    }
+    for (int i = 1; i <= N; ++i) cin >> Arr[i];
     
-    Init(1, N, 1);
+    Init(1, 1, N);
     
     for (int i = 0; i < M + K; ++i)
     {
@@ -65,13 +56,12 @@ int main()
         {
             ll Diff = b - Arr[a];
             Arr[a] = b;
-            Update(1, N, 1, a, Diff);
+            Update(1, 1, N, a, Diff);
         }
         else
         {
-            cout << Sum(1, N, a, b, 1) << '\n';
+            cout << Sum(1, 1, N, a, b) << '\n';
         }
     }
-
     return 0;
 }
