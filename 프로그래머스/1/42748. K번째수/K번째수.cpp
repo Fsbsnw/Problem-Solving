96a1bad6464f07866a1032b074962236218e1a7f
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2026-05-24 01:04:15 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    
+    for (vector<int>& command : commands)
+    {
+        int i = command[0];
+        int j = command[1];
+        int k = command[2];
+        
+        vector<int> copied(array.begin() + i - 1, array.begin() + j);
+        sort(copied.begin(), copied.end());
+        
+        answer.push_back(copied[k - 1]);
+    }
+    
+    return answer;
+}
